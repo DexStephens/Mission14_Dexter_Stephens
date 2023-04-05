13d197f3c9ccc16d67b3f5b84bb918b5e6e277f2
Dexter Stephens <dexterfstephens@gmail.com>
2023-04-04 18:17:34 -0600

    Add the new mission 14 content

---
+++ Mission14_API/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission14_API.Controllers.Data;
+using System.Collections;
+
+namespace Mission14_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        public MovieContext MovieContext { get; set; }
+        public MovieController(MovieContext movieContext) 
+        {
+            MovieContext = movieContext;
+        }
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            return MovieContext.Movies.Where(x => x.Edited == "Yes").OrderBy(x => x.Title);
+        }
+    }
+}
